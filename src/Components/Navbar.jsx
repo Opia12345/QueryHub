4c240f98184e338f225157e7cb964b0fc1268a14
Opia Precious <preciousopia7@gmail.com>
2025-05-11 20:47:38 +0100

    feat: added the mailto

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -19,9 +19,11 @@ const Navbar = () => {
 
   return (
     <nav className="flex justify-end items-center px-10 gap-4 py-8">
-      <button className="bg-pink-500 cursor-pointer hover:bg-pink-600 transition-all ease-in duration-300 px-4 py-2 rounded-md shadow-lg font-semibold text-white">
-        Contact Developer
-      </button>
+      <a href="mailto:preciousopia7@gmail.com">
+        <button className="bg-pink-500 cursor-pointer hover:bg-pink-600 transition-all ease-in duration-300 px-4 py-2 rounded-md shadow-lg font-semibold text-white">
+          Contact Developer
+        </button>
+      </a>
 
       <button className="cursor-pointer" onClick={toggleDarkMode}>
         {isDarkMode ? <Sun size={28} /> : <Moon size={28} />}
